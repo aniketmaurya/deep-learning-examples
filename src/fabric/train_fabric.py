94220c55d8cc93404dee7e70c496f96e029da173
Aniket Maurya <aniket@lightning.ai>
2023-03-21 03:46:19 -0400

    update model

--- src/fabric/train_fabric.py
+++ src/fabric/train_fabric.py
@@ -6,9 +6,10 @@ import torch.nn.functional as F
 import torchvision
 import torchvision.transforms as transforms
 from lightning.fabric import Fabric
+from timm import create_model
 from tqdm import tqdm
 
-fabric = Fabric(accelerator="auto", devices="auto", strategy="auto")
+fabric = Fabric(accelerator="auto", devices=1, strategy="auto")
 fabric.launch()
 
 
@@ -29,35 +30,10 @@ def load_data():
     return train_loader
 
 
-class Net(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1)  # flatten all dimensions except batch
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-
-    def training_step(self, batch, batch_idx):
-        x, y = batch
-        loss = self.common_step(x, y, "train")
-        return loss
-
-
 def main():
     train_loader = load_data()
 
-    model = Net()
+    model = model = create_model("resnet18", num_classes=10)
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
