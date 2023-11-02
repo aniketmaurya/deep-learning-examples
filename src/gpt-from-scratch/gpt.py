486eeb030fda1fe8d4d6112922a5aa59ccacaa93
Aniket Maurya <theaniketmaurya@gmail.com>
2023-11-03 00:46:30 +0530

    format

--- src/gpt-from-scratch/gpt.py
+++ src/gpt-from-scratch/gpt.py
@@ -48,6 +48,7 @@ def get_batch(split):
     y = torch.stack([data[i + 1 : i + block_size + 1] for i in idx]).to(device)
     return x, y
 
+
 @torch.no_grad()
 def estimate_loss():
     model.eval()
@@ -69,30 +70,33 @@ class Head(nn.Module):
         self.key = nn.Linear(n_embd, head_size, bias=False)
         self.query = nn.Linear(n_embd, head_size, bias=False)
         self.value = nn.Linear(n_embd, head_size, bias=False)
-        self.register_buffer("tril", torch.tril(torch.ones(block_size, block_size, device=device)))
+        self.register_buffer(
+            "tril", torch.tril(torch.ones(block_size, block_size, device=device))
+        )
 
         self.dropout = nn.Dropout(dropout=dropout)
-    
+
     def forward(self, x):
         B, T, C = x.shape
         k = self.key(x)  # B x T x head_size
         q = self.query(x)  # B x T x head_size
-        
+
         wei = q @ k.transpose(-2, -1) * C**-0.5  # (B,T,T)
-        wei = wei.masked_fill(self.tril[:T, :T]==0, float('-inf'))  # (B,T,T)
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float("-inf"))  # (B,T,T)
         wei = F.softmax(wei, dim=-1)  # (B, T, T)
         wei = self.dropout(wei)
         v = self.value(x)  # (B, T, head_size)
         out = wei @ v  # (B, T, head_size)
         return out
-    
+
+
 class FeedForward(nn.Module):
     def __init__(self, n_embd):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(n_embd, 4* n_embd),
+            nn.Linear(n_embd, 4 * n_embd),
             nn.ReLU(),
-            nn.Linear(4*n_embd, n_embd),
+            nn.Linear(4 * n_embd, n_embd),
             nn.Dropout(dropout),
         )
 
@@ -104,22 +108,24 @@ class MultiHeadAttention(nn.Module):
     def __init__(self, num_heads, head_size):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
-    
+
     def forward(self, idx):
         return torch.cat([head(idx) for head in self.heads], dim=-1)
+
 
 class Block(nn.Module):
     def __init__(self, n_embd, head_size):
         super().__init__()
-        self.sa_head = MultiHeadAttention(head_size, n_embd//head_size)
+        self.sa_head = MultiHeadAttention(head_size, n_embd // head_size)
         self.ffwd = FeedForward(n_embd)
         self.ln1 = nn.LayerNorm(n_embd)
         self.ln2 = nn.LayerNorm(n_embd)
-    
+
     def forward(self, x):  # (B,T,C)
         x = x + self.sa_head(self.ln1(x))  # (B,T,C)
         x = x + self.ffwd(self.ln2(x))  # (B,T,C)
         return x  # (B,T,C)
+
 
 class BigramLanguageModel(nn.Module):
     def __init__(self) -> None:
@@ -139,11 +145,13 @@ class BigramLanguageModel(nn.Module):
     def forward(self, idx, targets=None):
         B, T = idx.shape
         token_emb = self.emb_table(idx)  # B X T X C,  C => n_embd
-        positional_emb = self.positional_embd_table(torch.arange(T, device=device))  # TxC
+        positional_emb = self.positional_embd_table(
+            torch.arange(T, device=device)
+        )  # TxC
         x = token_emb + positional_emb
         x = self.block(x)
         logits = self.lm_head(x)  # (B, T, vocab_size)
-        
+
         B, T, C = logits.shape
         loss = None
         if targets is not None:  # targets -> B x T
@@ -169,10 +177,11 @@ optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 
 
 for iter in track(range(max_iter), description="Training..."):
-
-    if (iter+1) % eval_interval == 0:
+    if (iter + 1) % eval_interval == 0:
         losses = estimate_loss()
-        print(f"Step: {iter}, train loss: {losses['train']:.4f}, val loss: {losses['val']:.4f}")
+        print(
+            f"Step: {iter}, train loss: {losses['train']:.4f}, val loss: {losses['val']:.4f}"
+        )
 
     x, y = get_batch("train")
     logits, loss = model(x, y)
@@ -182,7 +191,7 @@ for iter in track(range(max_iter), description="Training..."):
 
 try:
     torch.save(model, "model.pt")
-except:pass
-context = torch.zeros((1,1), dtype=torch.long, device=device)
+except:
+    pass
+context = torch.zeros((1, 1), dtype=torch.long, device=device)
 print(decode(model.generate(context)[0].cpu().tolist()))
- 
