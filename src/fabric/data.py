42bd80eb51db1155418c16d4f175746370677d57
Aniket Maurya <theaniketmaurya@gmail.com>
2023-11-06 02:27:56 +0530

    update

---
+++ src/fabric/data.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+from lightning.fabric import Fabric
+from timm import create_model
+from lightning.fabric.plugins import TransformerEnginePrecision
+from tqdm import tqdm
+
+
+def load_data():
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    )
+
+    batch_size = 32
+
+    train_set = torchvision.datasets.CIFAR10(
+        root="~/data", train=True, download=True, transform=transform
+    )
+    train_loader = torch.utils.data.DataLoader(
+        train_set, batch_size=batch_size, shuffle=True, num_workers=4
+    )
+
+    return train_loader
+
---
+++ src/fabric/train_fabric_fp8_h100.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+from lightning.fabric import Fabric
+from timm import create_model
+from tqdm import tqdm
+from data import load_data
+
+
+# Select 8bit mixed precision via TransformerEngine, with model weights in bfloat16
+fabric = Fabric(precision="transformer-engine")
+fabric.launch()
+
+
+def main():
+    train_loader = load_data()
+
+    model = model = create_model("resnet50", num_classes=10)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+    # ⚡️⚡️⚡️⚡️⚡️ Setup model and optimizer with Fabric ⚡️⚡️⚡️⚡️⚡️
+    model, optimizer = fabric.setup(model, optimizer)
+    # setup dataloader with Fabric
+    train_loader = fabric.setup_dataloaders(train_loader)
+
+    for i in range(2):
+        for x, y in tqdm(train_loader):
+            optimizer.zero_grad()
+            logits = model(x)
+            loss = criterion(logits, y)
+            # ⚡️⚡️⚡️⚡️⚡️ fabric.backward(...) instead of loss.backward() ⚡️⚡️⚡️⚡️⚡️
+            fabric.backward(loss)
+            optimizer.step()
+
+
+if __name__ == "__main__":
+    main()
